6248d6c95a3b2ae650abf5f3b2ae2bddc04b4808
dangerousplay <gm.davi.gm@live.com>
2019-02-25 19:05:36 -0300

    Refatorando projeto e removendo package duplicado. Adicionando plugin de release do maven. Closes #3

--- pet-clinic-web/src/main/java/com/dangerousplay/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/dangerousplay/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,4 +1,4 @@
-package com.dangerousplay.sfgpetclinic.sfgpetclinic;
+package com.dangerousplay.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
