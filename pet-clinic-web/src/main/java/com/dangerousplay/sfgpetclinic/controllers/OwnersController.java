c3e06d7f1b5796ea528e1d3f39b7b92cd422b656
dangerousplay <gm.davi.gm@live.com>
2019-03-02 22:33:55 -0300

    Implementando listagem de Owners na página owners index. Closes #16

--- pet-clinic-web/src/main/java/com/dangerousplay/sfgpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/com/dangerousplay/sfgpetclinic/controllers/OwnersController.java
@@ -1,6 +1,8 @@
 package com.dangerousplay.sfgpetclinic.controllers;
 
+import com.dangerousplay.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,9 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/owners")
 public class OwnersController {
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
     @GetMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
